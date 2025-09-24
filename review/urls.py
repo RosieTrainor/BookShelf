ab31d5bf9292574df6e51da577e1247ca8e839e7
RosieTrainor <trainor.rosie@gmail.com>
2025-09-24 18:30:26 +0100

    Add view and basic template for all book reviews regardless of user

--- review/urls.py
+++ review/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from review import views as review_views
 
 urlpatterns = [
-    path('', review_views.ReviewList.as_view(), name='user_review_list'),
+    path('', review_views.AllReviewList.as_view(), name='index'),
+    path('my-reviews/', review_views.UserReviewList.as_view(), name='user_review_list'),
 ]
--- review/views.py
+++ review/views.py
@@ -6,10 +6,17 @@ from .models import Review
 # Create your views here.
 
 
-class ReviewList(LoginRequiredMixin, generic.ListView):
+class AllReviewList(generic.ListView):
+
+    queryset = Review.objects.all()
+    template_name = "review/index.html"
+    paginate_by = 6
+
+
+class UserReviewList(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Review.objects.filter(reviewer=self.request.user)
     
-    template_name = "review/review_list.html"
+    template_name = "review/user_review_list.html"
     paginate_by = 6
