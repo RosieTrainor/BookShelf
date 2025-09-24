dd47759c0b12c4169d8a3507a685577a2d6ac4e1
RosieTrainor <trainor.rosie@gmail.com>
2025-09-24 13:31:30 +0100

    Register models with admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
+from .models import Author
+from .models import Book
 
 # Register your models here.
+
+
+admin.site.register(Review)
+admin.site.register(Author)
+admin.site.register(Book)
