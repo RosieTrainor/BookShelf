ff6a810a7eea8cbbbc9e19ac7f9532362640ab41
RosieTrainor <trainor.rosie@gmail.com>
2025-09-30 18:13:08 +0100

    Add basic template for add_review form

--- review/urls.py
+++ review/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('<int:pk>', review_views.review_detail, name='review_detail'),
     path('my-reviews/', review_views.UserReviewList.as_view(),
          name='user_review_list'),
+    path('add-review/', review_views.add_review, name='add_review'),
     path('', review_views.AllReviewList.as_view(), name='index'),
 ]
