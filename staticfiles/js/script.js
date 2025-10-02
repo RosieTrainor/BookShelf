fcfa1fc13d3b3eaa9a0b56b7710155c948ad318e
RosieTrainor <trainor.rosie@gmail.com>
2025-10-02 14:48:48 +0100

    collectstatic for deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,9 +1,28 @@
-// Add review button
+document.addEventListener('DOMContentLoaded', function() {
+    handleAddReviewButton();
+    handleEditReviewButton();
+})
 
-const reviewButton = document.querySelector('#add-review');
-if (reviewButton) {
-    reviewButton.addEventListener('click', function () {
+// Add-review button
+function handleAddReviewButton() {
+    const reviewButton = document.querySelector('#add-review');
+    if (reviewButton) {
+        reviewButton.addEventListener('click', function () {
         window.location.href = '/add-review/';
     });
+    }
 }
-    
+
+// Edit-review button
+function handleEditReviewButton() {
+    const editButton = document.querySelector('#edit-review');
+    if (editButton) {
+        const reviewPk = editButton.dataset.reviewPk;
+        editButton.addEventListener('click', function () {
+            window.location.href = `/my-reviews/edit/${reviewPk}/`;
+        });
+    }
+}
+
+
+
