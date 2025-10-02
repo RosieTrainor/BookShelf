1918a1c8f9b3e1b0ad5a65c8fdb98c1078df43a4
RosieTrainor <trainor.rosie@gmail.com>
2025-10-02 17:28:50 +0100

    Delete unused (commented out) db from settings

--- book_shelf/settings.py
+++ book_shelf/settings.py
@@ -100,13 +100,6 @@ WSGI_APPLICATION = 'book_shelf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
