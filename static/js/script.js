e1dee7c68a685802c366c026b81e993d25cf34ec
RosieTrainor <trainor.rosie@gmail.com>
2025-10-02 16:40:40 +0100

    Add delete button with pop up modal to confirm. Add js to handle functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     handleAddReviewButton();
     handleEditReviewButton();
+    handleDeleteModal();
 })
 
 // Add-review button
@@ -21,6 +22,21 @@ function handleEditReviewButton() {
         editButton.addEventListener('click', function () {
             window.location.href = `/my-reviews/edit/${reviewPk}/`;
         });
+    }
+}
+
+// Trigger delete modal and set delete confirm button to delete review url
+function handleDeleteModal() {
+    const deleteModalButton = document.querySelector('#delete-review')
+    const deleteModal = new bootstrap.Modal(document.querySelector('#delete-modal'));
+    const deleteConfirmButton = document.querySelector('#delete-confirm');
+
+    if (deleteModalButton) {
+        const reviewPk = deleteModalButton.dataset.reviewPk;
+        deleteModalButton.addEventListener('click', function() {
+            deleteConfirmButton.setAttribute('href', `/my-reviews/delete/${reviewPk}/`);
+            deleteModal.show();
+        })
     }
 }
 
