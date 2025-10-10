79afb5abde620600b1186c102c3de46e99361d46
RosieTrainor <trainor.rosie@gmail.com>
2025-10-10 12:39:36 +0100

    Amend star rating template to load svgs from static rather than use bootstrap icons

--- review/templatetags/star_rating.py
+++ review/templatetags/star_rating.py
@@ -1,12 +1,42 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
+from functools import lru_cache
+import os
 
 
 register = template.Library()
 
 
+@lru_cache(maxsize=3)
+def load_svg(file_url):
+    """
+    Load the content of an SVG file from the static directory.
+    """
+    try:
+        file_path = os.path.join(settings.BASE_DIR, "static", file_url)
+        with open(file_path, "r", encoding="utf-8") as svg_file:
+            return svg_file.read()
+    except FileNotFoundError:
+        return None
+
+
 @register.filter
 def star_rating(value):
+    """
+    Generate the star rating HTML or return the rating value as fallback.
+    """
+    full_star_url = "icons/star-fill.svg"
+    half_star_url = "icons/star-half.svg"
+    empty_star_url = "icons/star.svg"
+
+    full_star_svg = load_svg(full_star_url)
+    half_star_svg = load_svg(half_star_url)
+    empty_star_svg = load_svg(empty_star_url)
+
+    if not full_star_svg or not half_star_svg or not empty_star_svg:
+        return f"Rating: {value}"
+
     full_star = 0
     half_star = 0
     empty_star = 0
@@ -19,10 +49,10 @@ def star_rating(value):
             empty_star += 1
     rating = []
     for star in range(full_star):
-        rating.append("<i class='bi bi-star-fill'></i>")
+        rating.append(full_star_svg)
     for star in range(half_star):
-        rating.append("<i class='bi bi-star-half'></i>")
+        rating.append(half_star_svg)
     for star in range(empty_star):
-        rating.append("<i class='bi bi-star'></i>")
+        rating.append(empty_star_svg)
 
     return mark_safe("".join(rating))
